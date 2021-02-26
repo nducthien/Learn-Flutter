f7e3cb07a985006a994db902f6f58fad6a23eb98
nducthien <thien.chotrax23@gmail.com>
2021-02-26 16:29:49 +0700

    create form login

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,8 +15,79 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  String _email = ''; // this is state
+  final emailEditingController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: "This is a state ful widget",
+      home: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.blueAccent,
+          title: Text('My App'),
+          centerTitle: true,
+          leading: IconButton(
+              icon: Icon(
+                Icons.list,
+                color: Colors.white,
+              ),
+              onPressed: () {}),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset('assets/pokemon.png'),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: TextField(
+                  // how to get value from textField ? use TextEditingController
+                  controller: emailEditingController,
+                  onChanged: (text) {
+                    // content change, it is run here
+                    this.setState(() {
+                      _email =
+                          text; // when state changed => build() rerun => reload widget
+                    });
+                  },
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(30))),
+                      labelText: 'Enter your email'),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Enter your password'),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                alignment: Alignment.centerRight,
+                child: Text(
+                  "Create account",
+                ),
+              ),
+              Text(
+                _email,
+                style: TextStyle(fontSize: 15, color: Colors.deepOrange),
+              ),
+              RaisedButton(
+                onPressed: () {},
+                textColor: Colors.blue,
+                padding: const EdgeInsets.all(0.0),
+                child: const Text('Login'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
